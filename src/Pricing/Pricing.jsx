ecb37d0377435dd6cc966e3790d2bab7d8f3310b
Jagjit singh <jagjitsingh.btcivil20@pec.edu.in>
2024-03-18 20:47:02 +0530

    feat: theme change and pricing screen

--- src/Pricing/Pricing.jsx
+++ src/Pricing/Pricing.jsx
@@ -1,133 +1,196 @@
-import React, { useEffect, useState } from 'react';
-import CheckCircleOutlineIcon from '@mui/icons-material/CheckCircleOutline';
+import React, { useState } from 'react';
+import Slider from '@mui/material/Slider';
+
+import Styles from "./Pricing.module.css";
 
 export default function Pricing() {
     return (
         <div style={{ position: "relative", width: "100%", display: "flex", flexDirection: "column", height: "100%", zIndex: 2 }}>
-            <div style={{ width: "100%", flex: 1, color: "white" }}>
-                <div style={{ display: "flex", flexDirection: "column", alignItems: "center", textAlign: "center" }}>
+            <div style={{ width: "90%", margin:"auto", flex: 1, color: "white" }}>
+                <div style={{ display: "flex", flexDirection: "column", alignItems: "center", textAlign: "center", gap: 10 }}>
                     <h1 style={{ fontSize: 45, fontWeight: 800 }}>
-                        Your First Month FREE with Claw's Exclusive Offer
+                        Find the Perfect Pricing Option for Your Legal Needs
                     </h1>
                     <div style={{ width: "70%" }}>
-                        <h5 style={{ fontSize: 25, fontWeight: 500 }}>
-                            Experience the Future of Legal Services Without Commitment – Dive into One Month of Claw, On the House!
-                        </h5>
+                        <h5 style={{ fontSize: 25, fontWeight: 500, color: "#777" }}>
+                            Explore our flexible pricing options designed to cater to a range of legal requirements. Select the plan that best fits your needs and budget.                        </h5>
                     </div>
                 </div>
-                <div style={{ display: "flex", flexDirection: "column", alignItems: "center", textAlign: "center", marginTop: 15 }}>
-                    <h1 style={{ margin: 0, fontWeight: 600 }}>
-                        <div>Free offer ends in!!</div>
-                        <Timer />
-                    </h1>
-                </div>
+
             </div>
-            <div style={{ marginTop: 50 }}>
-                <div style={{ width: "80%", margin: "auto", display: "flex", flexWrap: "wrap", justifyContent: 'space-around', gap: 16 }}>
-                    <PricingCard
-                        duration="Monthy"
-                        description="Get started with the fundamental tools you need for your legal journey at an affordable price"
-                        features={
-                            [
-                                { active: true, name: "Access to legalGPT" },
-                                { active: true, name: "Priority Search and Match" },
-                                { active: false, name: "Extended Profile Customization" },
-                                { active: false, name: "Secure Communication Channels" },
-                                { active: false, name: "Exclusive Pricing Insights" },
-                                { active: false, name: "Early Access to New Features" },
-                            ]
-                        }
-                    />
-                    <PricingCard
-                        duration="Yearly"
-                        description="Unlock premium features and enjoy a heightened level of legal support tailored to your specific needs"
-                        features={
-                            [
-                                { active: true, name: "Access to legalGPT" },
-                                { active: true, name: "Priority Search and Match" },
-                                { active: true, name: "Extended Profile Customization" },
-                                { active: true, name: "Secure Communication Channels" },
-                                { active: false, name: "Exclusive Pricing Insights" },
-                                { active: false, name: "Early Access to New Features" },
-                            ]
-                        }
-                    />
-                    <PricingCard
-                        duration="Enterprise"
-                        description="Tailored for those who demand the highest level of expertise and personalized legal assistance"
-                        features={
-                            [
-                                { active: true, name: "Access to legalGPT" },
-                                { active: true, name: "Priority Search and Match" },
-                                { active: true, name: "Extended Profile Customization" },
-                                { active: true, name: "Secure Communication Channels" },
-                                { active: true, name: "Exclusive Pricing Insights" },
-                                { active: true, name: "Early Access to New Features" },
-                            ]
-                        }
-                    />
+            <div style={{ marginTop: 50, width: "80%", margin: "auto" }}>
+                <div className={Styles.pricingContainer}>
+                    <div className={Styles.first}>
+                        <PricingCard
+                            duration="Monthy"
+                            sliderMap={monthly}
+
+                        />
+                    </div>
+                    <div className={Styles.second}>
+                        <PricingCard
+                            duration="Yearly"
+                            sliderMap={yearly}
+                        />
+                    </div>
+                    <div className={Styles.third}>
+                        <h1 style={{ color: "#008080", fontWeight: 800 }}>Enterprise</h1>
+                        <button style={{ backgroundColor: "#008080", color: "white", padding: "12px 40px", borderRadius: 10, border: "none", fontSize: 27 }}>Contact us</button>
+                    </div>
+                </div>
+                <div>
 
                 </div>
             </div>
-        </div>
+        </div >
     )
+}
+const monthly = {
+    request: {
+        marks: [
+            {
+                value: 0,
+                label: '1000'
+            },
+            {
+                value: 25,
+                label: '3000'
+            },
+            {
+                value: 50,
+                label: '5000'
+            },
+            {
+                value: 75,
+                label: '10000'
+            },
+            {
+                value: 100,
+                label: '20000'
+            },
+
+        ],
+        map: { 0: 1000, 25: 3000, 50: 5000, 75: 10000, 100: 20000 }
+    },
+    session: {
+        marks: [
+            { value: 0, label: 1 },
+            { value: 33, label: 2 },
+            { value: 66, label: 3 },
+            { value: 100, label: 4 },
+        ],
+        map: { 0: 1, 33: 2, 66: 3, 100: 4 }
+    }
+}
+const yearly = {
+    request: {
+        marks: [
+            {
+                value: 0,
+                label: '12000'
+            },
+            {
+                value: 25,
+                label: '36000'
+            },
+            {
+                value: 50,
+                label: '60000'
+            },
+            {
+                value: 75,
+                label: '150000'
+            },
+            {
+                value: 100,
+                label: '300000'
+            },
+
+        ],
+        map: { 0: 12000, 25: 36000, 50: 60000, 75: 150000, 100: 300000 }
+    },
+    session: {
+        marks: [
+            { value: 0, label: 1 },
+            { value: 33, label: 2 },
+            { value: 66, label: 3 },
+            { value: 100, label: 4 },
+        ],
+        map: { 0: 1, 33: 2, 66: 3, 100: 4 }
+    }
 }
 
 
-const PricingCard = ({ duration, description, features, link }) => {
-    return <div style={{ width: 350, border: "1px solid white", backgroundColor: "white", color: "black", borderRadius: 8, padding: 16, display: "flex", flexDirection: "column", gap: 16 }}>
-        <div style={{ display: "flex", flexDirection: "column", gap: 8 }}>
-            <h3 style={{ fontSize: 20, fontWeight: 700, margin: 0 }}>{duration}</h3>
-            <h4 style={{ fontSize: 16, fontWeight: 500, color: "#777", margin: 0 }}>{description}</h4>
+const PricingCard = ({ duration, sliderMap }) => {
+    const [request, setRequest] = useState();
+    const [session, setSession] = useState();
+
+    return <div style={{ border: "1px solid white", backgroundColor: "white", color: "black", borderRadius: 8, padding: 16, display: "flex", flexDirection: "column", gap: 16 }}>
+        <div style={{ display: "flex", flexDirection: "column", gap: 8, alignItems: "center" }}>
+            <h3 style={{ fontSize: 45, color: "#008080", fontWeight: 700, margin: 0 }}>{duration}</h3>
         </div>
-        <div>
-            <h2 style={{ fontSize: 36, fontWeight: 700, margin: 0 }}>199</h2>
+        <div style={{ width: "80%", margin: "auto", textAlign: "center" }}>
+            <div>
+                <h3 style={{ fontSize: 27 }}>Request</h3>
+                <Slider
+                    defaultValue={0}
+                    step={null}
+                    style={{ color: "#008080" }}
+                    marks={sliderMap.request.marks}
+                    valueLabelDisplay="off"
+                    onChange={(e) => setRequest(sliderMap.request.map[e.target.value])}
+                />
+            </div>
+            <div style={{ width: "80%", margin: "auto" }}>
+                <h3 style={{ fontSize: 27 }}>Users/Sessions</h3>
+                <Slider
+                    defaultValue={0}
+                    style={{ color: "#008080" }}
+                    step={null}
+                    onChange={(e) => setSession(sliderMap.session.map[e.target.value])}
+                    marks={sliderMap.session.marks}
+                    valueLabelDisplay="off"
+                />
+            </div>
         </div>
-        <div style={{ display: "flex", flexDirection: "column", gap: 16 }}>
-            {features.map(({ active, name }) => {
-                if (!active) {
-                    return <div style={{ display: "flex", gap: 8, fontSize: 16, fontWeight: 500, color: "#777" }}>
-                        <CheckCircleOutlineIcon />
-                        <div>{name}</div>
-                    </div>
-                }
-                else return <div style={{ display: "flex", gap: 8, fontSize: 16 }}>
-                    <CheckCircleOutlineIcon style={{ color: "green" }} />
-                    <div>{name}</div>
-                </div>
-            })}
+        <div style={{ display: "flex", alignItems: "center", flexDirection: "column", gap: 8 }}>
+            <h5>You're paying</h5>
+            <div>
+                <span style={{ marginRight: 10 }}>₹</span>
+                <span style={{ padding: "10px 50px", border: "1px solid #008080", borderRadius: 10 }}>{123}</span>
+            </div>
         </div>
-        <div>
-            <button style={{ width: "100%", padding: "14px 0px", border: "none", backgroundColor: "#8940FF", borderRadius: 8, color: "white", fontSize: 16 }}>
-                Get Started
+        <div style={{ display: "flex", justifyContent: "center", paddingTop: 35 }}>
+            <button style={{ padding: "16px 44px", border: "none", backgroundColor: "#008080", borderRadius: 8, color: "white", fontSize: 27 }}>
+                Get it now
             </button>
         </div>
     </div>
 }
 
-const Timer = () => {
-    const [days, setDays] = useState(0);
-    const [hours, setHours] = useState(0);
-    const [minutes, setMinutes] = useState(0);
-    const [seconds, setSeconds] = useState(0);
+// const Timer = () => {
+//     const [days, setDays] = useState(0);
+//     const [hours, setHours] = useState(0);
+//     const [minutes, setMinutes] = useState(0);
+//     const [seconds, setSeconds] = useState(0);
 
-    const deadline = "March, 31, 2024";
+//     const deadline = "March, 31, 2024";
 
-    const getTime = () => {
-        const time = Date.parse(deadline) - Date.now();
+//     const getTime = () => {
+//         const time = Date.parse(deadline) - Date.now();
 
-        setDays(Math.floor(time / (1000 * 60 * 60 * 24)));
-        setHours(Math.floor((time / (1000 * 60 * 60)) % 24));
-        setMinutes(Math.floor((time / 1000 / 60) % 60));
-        setSeconds(Math.floor((time / 1000) % 60));
-    }
+//         setDays(Math.floor(time / (1000 * 60 * 60 * 24)));
+//         setHours(Math.floor((time / (1000 * 60 * 60)) % 24));
+//         setMinutes(Math.floor((time / 1000 / 60) % 60));
+//         setSeconds(Math.floor((time / 1000) % 60));
+//     }
 
-    useEffect(() => {
-        const interval = setInterval(() => getTime(), 1000);
-        return () => clearInterval(interval);
-    }, [])
+//     useEffect(() => {
+//         const interval = setInterval(() => getTime(), 1000);
+//         return () => clearInterval(interval);
+//     }, [])
 
-    return <div>
-        {days} : {hours} : {minutes} : {seconds}
-    </div>
-}
+//     return <div>
+//         {days} : {hours} : {minutes} : {seconds}
+//     </div>
+// }
--- src/RootLayout/RootLayout.jsx
+++ src/RootLayout/RootLayout.jsx
@@ -6,7 +6,7 @@ import FooterBanner from '../FooterBanner/FooterBanner';
 export default function RootLayout({onClickFeatures}) {
     return (
         <div style={{ position: "relative", backgroundColor: "#13161f", width: "100%" }}>
-            <div style={{ position: "absolute", marginLeft: "calc(50vw - 400px)", height: 943, width: 761, background: "radial-gradient(circle, rgba(137, 64, 255,0.45) 0%, rgba(137, 64, 255, 0.15) 65%)", boxShadow: "0 0 100px 100px rgba(137, 64, 255, 0.15)", top: "-500px", borderRadius: 500 }} />
+            <div style={{ position: "absolute", marginLeft: "calc(50vw - 400px)", height: 943, width: 761, background: "radial-gradient(circle, rgba(0, 128, 128,0.45) 0%, rgba(0, 128, 128, 0.15) 65%)", boxShadow: "0 0 100px 100px rgba(0, 128, 128, 0.15)", top: "-500px", borderRadius: 500 }} />
             <Header onClickFeatures={onClickFeatures} />
             <Outlet />
             <div style={{ position: "relative" }}>
