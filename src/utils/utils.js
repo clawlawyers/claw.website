40045c385a825d72485b99fbf7af850a3f907aa5
clawlawyers <156838081+clawlawyers@users.noreply.github.com>
2024-05-30 19:16:02 +0530

    Update utils.js

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,2 +1,2 @@
 export const FLASK_API_ENDPOINT = process.env.NODE_ENV === "production" ? "https://gpt.clawlaw.in/api/v1" : "http://localhost:5000/api/v1";
-export const NODE_API_ENDPOINT = process.env.NODE_ENV === "production" ? "https://claw-backend.onrender.com/api/v1" : "http://localhost:8000/api/v1";
+export const NODE_API_ENDPOINT = process.env.NODE_ENV === "production" ? "https://claw-app-dev.onrender.com/api/v" : "http://localhost:8000/api/v1";
