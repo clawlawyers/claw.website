e1820ed9b1032496de25c9819ae45182c2c5b29a
ShubhamP528 <shubham528prajapati@gmail.coom>
2024-06-13 10:32:29 +0530

    gpt token bug fixed

--- src/Gpt/UserSessions.jsx
+++ src/Gpt/UserSessions.jsx
@@ -1,55 +1,72 @@
-import React, { useEffect, useState } from 'react'
-import { Link, useParams } from 'react-router-dom';
-import CircularProgress from '@mui/material/CircularProgress';
-import { NODE_API_ENDPOINT } from '../utils/utils';
-
+import React, { useEffect, useState } from "react";
+import { Link, useParams } from "react-router-dom";
+import CircularProgress from "@mui/material/CircularProgress";
+import { NODE_API_ENDPOINT } from "../utils/utils";
 
 export function UserSessions({ jwt, model }) {
-    const [isLoading, setIsLoading] = useState();
-    const [sessions, setSessions] = useState([]);
-    const { sessionId } = useParams();
+  const [isLoading, setIsLoading] = useState();
+  const [sessions, setSessions] = useState([]);
+  const { sessionId } = useParams();
 
-    useEffect(() => {
-        async function fetchUserSessions() {
-            try {
-                setIsLoading(true);
-                const res = await fetch(`${NODE_API_ENDPOINT}/gpt/sessions/${model}`, {
-                    method: "GET",
-                    headers: {
-                        Authorization: `Bearer ${jwt}`,
-                        "Content-Type": "application/json",
-                    }
-                });
-                const { data } = await res.json();
-                setSessions(data);
-            } catch (error) {
-                console.log(error);
-            } finally {
-                setIsLoading(false);
-            }
-        }
-        fetchUserSessions();
-    }, [jwt, model])
-    return (
-        <div style={{ height: "100%" }}>
-            {isLoading ?
-                <div style={{ display: "flex", height: "100%", justifyContent: "center", alignItems: "center" }}>
-                    <CircularProgress style={{ color: "white" }} />
-                </div>
-                :
-                <div >
-                    {sessions.map(({ name, id }) => (
-
-                        <Link
-                            key={id}
-                            style={{ textDecoration: "none", display: "block", color: "white", border: "none", whiteSpace: "nowrap", textOverflow: "ellipsis", overflow: "hidden", padding: 12, gap: 15, borderRadius: 10, backgroundColor: (sessionId === id ? "#777" : "inherit") }}
-                            to={`session/${id}`}>
-                            {name}
-                        </Link>
-
-                    ))}
-                </div>
-            }
-        </div >
-    )
+  useEffect(() => {
+    async function fetchUserSessions() {
+      try {
+        setIsLoading(true);
+        const res = await fetch(`${NODE_API_ENDPOINT}/gpt/sessions/${model}`, {
+          method: "GET",
+          headers: {
+            Authorization: `Bearer ${jwt}`,
+            "Content-Type": "application/json",
+          },
+        });
+        const { data } = await res.json();
+        setSessions(data);
+      } catch (error) {
+        console.log(error);
+      } finally {
+        setIsLoading(false);
+      }
+    }
+    fetchUserSessions();
+  }, [jwt, model]);
+  return (
+    <div style={{ height: "100%" }}>
+      {isLoading ? (
+        <div
+          style={{
+            display: "flex",
+            height: "100%",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <CircularProgress style={{ color: "white" }} />
+        </div>
+      ) : (
+        <div>
+          {sessions.map(({ name, id }) => (
+            <Link
+              key={id}
+              style={{
+                textDecoration: "none",
+                display: "block",
+                color: "white",
+                border: "none",
+                whiteSpace: "nowrap",
+                textOverflow: "ellipsis",
+                overflow: "hidden",
+                padding: 12,
+                gap: 15,
+                borderRadius: 10,
+                backgroundColor: sessionId === id ? "#777" : "inherit",
+              }}
+              to={`session/${id}`}
+            >
+              {name}
+            </Link>
+          ))}
+        </div>
+      )}
+    </div>
+  );
 }
--- src/features/gpt/gptSlice.js
+++ src/features/gpt/gptSlice.js
@@ -1,21 +1,23 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import { NODE_API_ENDPOINT } from "../../utils/utils";
 
-
-export const generateResponse = createAsyncThunk('gpt/generateResponse', async ({ sessionId, model }, callback) => {
+export const generateResponse = createAsyncThunk(
+  "gpt/generateResponse",
+  async ({ sessionId, model }, callback) => {
     const { auth, gpt } = callback.getState();
 
     const { jwt } = auth.user;
     const { prompt, token } = gpt;
-    if (token.used >= token.total) throw new Error("Not enough tokens, please upgrade or try again later!")
+    if (token.used >= token.total || token.used + 1 >= token.total)
+      throw new Error("Not enough tokens, please upgrade or try again later!");
     const res = await fetch(`${NODE_API_ENDPOINT}/gpt/session/prompt`, {
-        method: "POST",
-        body: JSON.stringify({ sessionId, prompt, model }),
-        headers: {
-            Authorization: `Bearer ${jwt}`,
-            "Content-Type": "application/json",
-        }
-    })
+      method: "POST",
+      body: JSON.stringify({ sessionId, prompt, model }),
+      headers: {
+        Authorization: `Bearer ${jwt}`,
+        "Content-Type": "application/json",
+      },
+    });
     return await res.json();
 
     // else {
@@ -29,64 +31,65 @@ export const generateResponse = createAsyncThunk('gpt/generateResponse', async (
     //     })
     //     return await res.json();
     // }
-});
-
+  }
+);
 
 export const gptSlice = createSlice({
-    name: "gpt",
-    initialState: {
-        status: "idle",
-        prompt: null,
-        response: null,
-        relatedCases: {
-            messageId: null,
-            cases: [],
-        },
-        token: {
-            used: null,
-            total: null,
-        },
-        plan: null,
-        error: null,
+  name: "gpt",
+  initialState: {
+    status: "idle",
+    prompt: null,
+    response: null,
+    relatedCases: {
+      messageId: null,
+      cases: [],
     },
-    reducers: {
-        setGpt: (state, action) => {
-            state.status = "idle";
-            state.prompt = action.payload.prompt;
-            state.response = null;
-        },
-        resetGpt: (state) => {
-            state.response = null;
-            state.status = "idle";
-            state.prompt = null;
-        },
-        setToken: (state, action) => {
-            state.token = action.payload.token;
-        },
-        setPlan: (state, action) => {
-            state.plan = action.payload.plan;
-        },
-        setRelatedCases: (state, action) => {
-            state.relatedCases.messageId = action.payload.messageId;
-            state.relatedCases.cases = action.payload.cases;
-        }
+    token: {
+      used: null,
+      total: null,
     },
-    extraReducers: (builder) => {
-        builder.addCase(generateResponse.pending, (state) => {
-            state.status = "pending";
-        })
-        builder.addCase(generateResponse.fulfilled, (state, action) => {
-            state.response = action.payload.data;
-            state.token = action.payload.data?.token;
-            state.status = "succeeded";
-        })
-        builder.addCase(generateResponse.rejected, (state, action) => {
-            state.status = "failed";
-            state.error = action.error;
-        })
-    }
+    plan: null,
+    error: null,
+  },
+  reducers: {
+    setGpt: (state, action) => {
+      state.status = "idle";
+      state.prompt = action.payload.prompt;
+      state.response = null;
+    },
+    resetGpt: (state) => {
+      state.response = null;
+      state.status = "idle";
+      state.prompt = null;
+    },
+    setToken: (state, action) => {
+      state.token = action.payload.token;
+    },
+    setPlan: (state, action) => {
+      state.plan = action.payload.plan;
+    },
+    setRelatedCases: (state, action) => {
+      state.relatedCases.messageId = action.payload.messageId;
+      state.relatedCases.cases = action.payload.cases;
+    },
+  },
+  extraReducers: (builder) => {
+    builder.addCase(generateResponse.pending, (state) => {
+      state.status = "pending";
+    });
+    builder.addCase(generateResponse.fulfilled, (state, action) => {
+      state.response = action.payload.data;
+      state.token = action.payload.data?.token;
+      state.status = "succeeded";
+    });
+    builder.addCase(generateResponse.rejected, (state, action) => {
+      state.status = "failed";
+      state.error = action.error;
+    });
+  },
 });
 
-export const { resetGpt, setGpt, setPlan, setToken, setRelatedCases } = gptSlice.actions;
+export const { resetGpt, setGpt, setPlan, setToken, setRelatedCases } =
+  gptSlice.actions;
 
 export default gptSlice.reducer;
