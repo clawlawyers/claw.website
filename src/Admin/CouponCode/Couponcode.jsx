850d6808984f872a4863690a865ec3e7ff2bcaf0
Vivek Ranjan Sahoo <vivekranjansahoo81@gmail.com>
2024-06-25 17:05:12 +0530

    admin panel coupon code

--- src/Admin/CouponCode/Couponcode.jsx
+++ src/Admin/CouponCode/Couponcode.jsx
@@ -61,58 +61,102 @@ const Couponcode = () => {
     }
   };
   return (
-    <div style={{ position: "absolute" }}>
-      <h2>Create Coupon</h2>
-      <input
-        type="text"
-        value={code}
-        onChange={(e) => setCode(e.target.value)}
-      />
-      <input
-        type="number"
-        value={discount}
-        onChange={(e) => setDiscount(e.target.value)}
-      />
-      <input
-        type="date"
-        value={expirationDate}
-        onChange={(e) => setExpirationDate(e.target.value)}
-      />
-      <button onClick={createCoupon} style={{ backgroundColor: "yellow" }}>
-        Create
-      </button>
+    <>
+      <h2 className="text-2xl font-bold mb-4">All Coupons</h2>
+      <div className="container mx-auto p-4">
+        <div
+          style={{
+            position: "sticky",
+            width: "300px",
+            marginLeft: "80px",
+          }}
+        >
+          <h2>Create Coupon</h2>
+          <input
+            type="text"
+            value={code}
+            onChange={(e) => setCode(e.target.value)}
+            placeholder="Name"
+          />
+          <br />
 
-      <h2>Deactivate Coupon</h2>
-      <input
-        type="text"
-        value={deactivateCode}
-        onChange={(e) => setDeactivateCode(e.target.value)}
-      />
-      <button onClick={deactivateCoupon} style={{ backgroundColor: "yellow" }}>
-        Deactivate
-      </button>
-      <br />
+          <input
+            type="number"
+            value={discount}
+            onChange={(e) => setDiscount(e.target.value)}
+            placeholder="discount"
+          />
+          <br />
+          <input
+            type="date"
+            value={expirationDate}
+            onChange={(e) => setExpirationDate(e.target.value)}
+          />
+          <br />
+          <button onClick={createCoupon} style={{ backgroundColor: "yellow" }}>
+            Create
+          </button>
+          <br />
+          <br />
 
-      <h2>Delete Coupon</h2>
-      <input
-        type="text"
-        value={deleteCode}
-        onChange={(e) => setDeleteCode(e.target.value)}
-      />
-      <button onClick={deleteCoupon} style={{ backgroundColor: "red" }}>
-        Delete
-      </button>
-      <h2>All Coupons</h2>
-      <ul>
-        {coupons.map((coupon) => (
-          <li key={coupon._id}>
-            Code: {coupon.code}, Discount: {coupon.discount}%, Expiration Date:{" "}
-            {new Date(coupon.expirationDate).toLocaleDateString()}, Active:{" "}
-            {coupon.isActive ? "Yes" : "No"}
-          </li>
-        ))}
-      </ul>
-    </div>
+          <h2>Deactivate Coupon</h2>
+          <input
+            type="text"
+            value={deactivateCode}
+            onChange={(e) => setDeactivateCode(e.target.value)}
+          />
+          <button
+            onClick={deactivateCoupon}
+            style={{ backgroundColor: "yellow" }}
+          >
+            Deactivate
+          </button>
+          <br />
+
+          <h2>Delete Coupon</h2>
+          <input
+            type="text"
+            value={deleteCode}
+            onChange={(e) => setDeleteCode(e.target.value)}
+          />
+          <button onClick={deleteCoupon} style={{ backgroundColor: "red" }}>
+            Delete
+          </button>
+        </div>
+        <table
+          className="table-auto w-full mb-4"
+          style={{
+            overflow: "auto",
+            color: "yellow",
+            marginLeft: "100px",
+            position: "sticky",
+          }}
+        >
+          <thead>
+            <tr>
+              <th className="px-4 py-2">Code</th>
+              <th className="px-4 py-2">Discount</th>
+              <th className="px-4 py-2">Expiration Date</th>
+              <th className="px-4 py-2">Active</th>
+            </tr>
+          </thead>
+          <tbody>
+            {coupons.map((coupon) => (
+              <tr key={coupon._id}>
+                <td className="border px-4 py-2">{coupon.code}</td>
+                <td className="border px-4 py-2">{coupon.discount}%</td>
+                <td className="border px-4 py-2">
+                  {new Date(coupon.expirationDate).toLocaleDateString()}
+                </td>
+                <td className="border px-4 py-2">
+                  {coupon.isActive ? "Yes" : "No"}
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </>
   );
 };
 
