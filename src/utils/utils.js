4be95181edd821e41bf1653e82b410b94a00d8f0
Shubham Prajapati <shubham528prajapati@gmail.com>
2024-11-30 16:33:44 +0530

    node api change

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -5,30 +5,24 @@ export const FLASK_API_ENDPOINT =
 
 export const NODE_API_ENDPOINT =
   process.env.NODE_ENV === "production"
-    ? // ? "https://claw-app-dev.onrender.com/api/v1"
-      "https://claw-backend.onrender.com/api/v1"
+    ? "https://claw-app-dev.onrender.com/api/v1"
     : "http://localhost:8000/api/v1";
 
 export const ADIRA_ENDPOINT =
   process.env.NODE_ENV === "production"
     ? // ? "https://claw-app-dev.onrender.com/api/v1"
-   "https://adira.clawlaw.in"
+      "https://adira.clawlaw.in"
     : "http://localhost:5000";
 
 export const OTP_ENDPOINT =
   process.env.NODE_ENV === "production"
-    ? 
-   "https://claw-app.onrender.com"
+    ? "https://claw-app.onrender.com"
     : "http://localhost:7000";
 
 export const WARROOM_ENDPOINT =
-process.env.NODE_ENV === "production"
-      ? // ? "https://claw-app-dev.onrender.com/api/v1"
-     "https://courtroom-freetrial-dev.netlify.app/"
-      : "http://localhost:3001/";
+  process.env.NODE_ENV === "production"
+    ? // ? "https://claw-app-dev.onrender.com/api/v1"
+      "https://courtroom-freetrial-dev.netlify.app/"
+    : "http://localhost:3001/";
 
 // export const OTP_ENDPOINT = "https://claw-app.onrender.com"
-   
-
-
-
