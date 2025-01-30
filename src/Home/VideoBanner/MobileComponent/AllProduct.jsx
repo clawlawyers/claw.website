30f50a473041e0c2c1a2b22574ffdd93b16ce01d
rahul9369 <rp25082003@gmail.com>
2025-01-30 16:46:36 +0530

    response

--- src/Home/VideoBanner/MobileComponent/AllProduct.jsx
+++ src/Home/VideoBanner/MobileComponent/AllProduct.jsx
@@ -101,7 +101,7 @@ const allproduct = () => {
         </div>
 
         <div
-          className="bg-[rgba(0,128,128,0.2)] border-2 border-[#00C37B] sm:pt-6 rounded-lg flex flex-col items-center justify-between shadow-lg cursor-pointer transition-all hover:scale-95 duration-300 mx-1 w-[100px] h-[110px] sm:w-52 sm:h-52 "
+          className="bg-[rgba(0,128,128,0.2)] border-2 border-[#00C37B] sm:pt-6 rounded-lg flex flex-col items-center justify-between shadow-lg cursor-pointer transition-all hover:scale-95 duration-300 sm:mx-1 ml-12 mr-4 w-[100px] h-[110px] sm:w-52 sm:h-52 "
           onClick={openWarrrom}>
           <img
             src={Warroom}
@@ -119,7 +119,7 @@ const allproduct = () => {
 
         <div
           onClick={handleCasePrediction}
-          className="bg-[rgba(0,128,128,0.2)] border-2 mx-1 border-green-400 hover:scale-95 cursor-pointer transition-all duration-300 text-green-400 rounded-lg flex flex-col items-center justify-center  sm:pt-6 shadow-lg w-[100px] h-[110px]  sm:w-52 sm:h-52">
+          className="bg-[rgba(0,128,128,0.2)] border-2 sm:mx-1 ml-12 border-green-400 hover:scale-95 cursor-pointer transition-all duration-300 text-green-400 rounded-lg flex flex-col items-center justify-center  sm:pt-6 shadow-lg w-[100px] h-[110px]  sm:w-52 sm:h-52">
           <img
             src={CasePrediction}
             className="w-8 pt-4  h-12  sm:w-16 sm:h-16 sm:pt-0 mb-4 "
